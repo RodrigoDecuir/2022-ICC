38e497cef7f9403986810e2d9aa630b6f29fff7c
Rodrigo Andre Decuir Fuentes <ojo@ciencias.unam.mx>
2022-11-26 11:28:35 -0600

    Clase libro lista

--- Tareas/03_Tarea/src/Librero.java
+++ Tareas/03_Tarea/src/Librero.java
@@ -4,55 +4,54 @@
  * @version 1.0
  */
 public class Librero {
-    public static void main(String[]args) {
-        
-        //Libro     
-        /*ATRIBUTOS*/
-       // Libro[] libros = new Libro[];
+            
+    /*ATRIBUTOS*/
        
-        /*CONSTRUCTORES*/
-        public Librero() {
+    /*CONSTRUCTORES*/
         
+    /*METODOS*/
+    /*acceso*/
+        
+    /*modificacion*/
+        
+    /*llenar librero*/
+
+    /*vaciar librero*/
+
+    /*agregar un libro*/
+
+    /*eliminar un libro*/
+
+    /*buscar un libro(recursivo)*/
+    /**
+    * Metodo recursivo para localizar un dato en un arreglo ordenado
+    * @param datos -- arreglo de enteros en donde esta el espacio de busqueda
+    * @param inicio -- posicion inicial para la busqueda
+    * @param fin -- posicion final para la busqueda
+    * @param buscado -- elemento a buscar
+    * @return int -- posicion del elemento encontrado o -1 si no esta
+    */
+    public int busquedaBinaria(int[] datos, int inicio, int fin, int buscado) {
+        int mitad = (inicio + fin)/2;
+        if(inicio > fin) { 
+            return false;
+        } else if(datos[mitad] < buscado) {
+            return busquedaBinaria(a, mitad + 1, fin, buscado);
+        } else if(datos[mitad] > buscado) {
+            return busquedaBinaria(a, inicio, mitad - 1, buscado);
+        } else {
+            return mitad; //Lo encontro
         }
-        /*METODOS*/
-        /*acceso*/
-        
-        /*modificacion*/
-        
-        /*llenar librero*/
-
-        /*vaciar librero*/
-
-        /*agregar un libro*/
-
-        /*eliminar un libro*/
-
-        /*buscar un libro(recursivo)*/
-        /**
-         * Metodo recursivo para localizar un dato en un arreglo ordenado
-         * @param datos -- arreglo de enteros en donde esta el espacio de busqueda
-         * @param inicio -- posicion inicial para la busqueda
-         * @param fin -- posicion final para la busqueda
-         * @param buscado -- elemento a buscar
-         * @return int -- posicion del elemento encontrado o -1 si no esta
-         */
-        public int busquedaBinaria(int[] datos, int inicio, int fin, int buscado) {
-            int mitad = (inicio + fin)/2;
-            if(inicio > fin) 
-                return false;
-            else if(datos[mitad] < buscado)
-                return busquedaBinaria(a, mitad + 1, fin, buscado);
-            else if(datos[mitad] > buscado)
-                return busquedaBinaria(a, inicio, mitad - 1, buscado);
-            else
-                return mitad; //Lo encontro
-        }
-
-        /*ordenar por numero de paginas(recursivo)*/
-        
-        /*ordenar por fecha de publicacion(recursivo)*/
-
-        /*toString*/
-
     }
+
+    /*ordenar por numero de paginas(recursivo)*/
+        
+    /*ordenar por fecha de publicacion(recursivo)*/
+
+    /*toString*/
+
+    public static void main(String[]args) {
+    
+    }
+
 }  
--- Tareas/03_Tarea/src/Libro.java
+++ Tareas/03_Tarea/src/Libro.java
@@ -5,22 +5,102 @@
  */
 public class Libro {
     /*ATRIBUTOS*/
-    String titulo;
-    String autor;
-    int numeroDePaginas;
-    int fechaPublicacion;
+    private String titulo;
+    private String autor;
+    private int numeroDePaginas;
+    private int fechaDePublicacion;
     /*CONSTRUCTORES*/
+    /**
+     * Constructor que inicializa un libro vacio.
+     */
     public Libro() {
-        
+       titulo = null; 
+       autor = null; 
+       numeroDePaginas = 0;
+       fechaDePublicacion = 0; 
     }
-    //public Libro( ) {
-
-    //}
+    /**
+     * Constructor que inicializa un libro con parametros.
+     * @param titulo -- titulo del libro. 
+     * @param autor -- autor del libro. 
+     * @param numeroDePaginas -- numero de paginas del libro.
+     * @param fechaDePublicacion -- fecha de publicacion del libro.
+     */ 
+    public Libro(String titulo, String autor, int numeroDePaginas, int fechaDePublicacion) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroDePaginas = numeroDePaginas;
+        this.fechaDePublicacion = fechaDePublicacion;
+    }
     /*METODOS*/
     /*acceso*/
-        
+    /**
+     * Metodo que obtiene el titulo de un libro.
+     * @return String -- titulo del libro.
+     */
+    public String obtenerTitulo() { 
+        return titulo; 
+    }
+    /**
+     * Metodo que obtiene el autor de un libro.
+     * @return String -- autor del libro.
+     */
+    public String obtenerAutor() {
+        return autor; 
+    }
+    /**
+     * Metodo que obtiene el numero de paginas de un libro.
+     * @return int -- numero de paginas del libro.
+     */
+    public int obtenerNumeroDePaginas() {
+        return numeroDePaginas; 
+    } 
+    /**
+     * Metodo que obtiene la fecha de publicacion de un libro.
+     * @return int -- fecha de publicacion del libro.
+     */
+    public int obtenerFechaDePublicacion() {
+        return fechaDePublicacion; 
+    }   
     /*modificacion*/
+    /**
+     * Metodo para asignar el titulo a un libro.
+     * @param String -- nuevo titulo del libro.
+     */
+    public void asignarTitulo(String titulo) { 
+        this.titulo = titulo;
+    } 
+    /**
+     * Metodo para asignar el autor a un libro.
+     * @param String -- nuevo autor del libro.
+     */
+    public void asignarAutor(String autor) {
+        this.autor = autor;
+    }
+    /**
+     * Metodo para asignar numero de paginas a un libro.
+     * @param int -- nuevo numero de paginas del libro.
+     */
+    public void asignarNumeroDePaginas(int numeroDePaginas) {
+        this.numeroDePaginas = numeroDePaginas;
+    }
+    /**
+     * Metodo para asignar fecha de publicacion a un libro.
+     * @param int -- nueva fecha de publicacion del libro.
+     */
+    public void asignarFechaDePublicacion(int fechaDePublicacion) {
+        this.fechaDePublicacion = fechaDePublicacion; 
+    }
 
     /*toString*/
-
+    /**
+     * Metodo que devuelve la informacion de un libro.
+     * @return string -- informacion del libro.
+     */
+    public String toString() {
+        return "Titulo: " + titulo + "\n" +
+               "Autor: " + autor + "\n" + 
+               "N. pag: " + numeroDePaginas + "\n" + 
+               "Fecha de publicacion: " + fechaDePublicacion;  
+    }
 }  

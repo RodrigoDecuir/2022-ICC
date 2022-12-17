91e71667f52a086e3a0ab9d1623c768ba4e4cf39
Rodrigo Andre Decuir Fuentes <ojo@ciencias.unam.mx>
2022-12-16 19:42:13 -0600

    setting up the classes

---
+++ Proyecto_final/src/main/java/mx/unam/ciencias/icc/Agencia.java
@@ -0,0 +1,12 @@
+public class Agencia {
+    private int fechaDeSalida;
+    private int fechaDeRegreso;
+    Avion[] a = new Avion[];
+    HabitacionSimple[] hs = new HabitacionSimple[];
+    HabitacionFamiliar[] hf = new HabitacionFamiliar[];
+    public reservar();
+    public confirmarVuelo();
+    public buscarReservacion();
+    public verTodasLasReservaciones();
+    public consultarInformacionDelAvion();
+}
--- Proyecto_final/src/main/java/mx/unam/ciencias/icc/Menu.java
+++ Proyecto_final/src/main/java/mx/unam/ciencias/icc/Menu.java
@@ -6,3 +6,6 @@
 
 package mx.unam.ciencias.icc;
 
+public class Menu {
+
+}
